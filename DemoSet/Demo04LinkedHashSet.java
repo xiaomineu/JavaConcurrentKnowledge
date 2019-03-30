d5a95d253add2e4f878ec041b88043737fa39bf8
xiaomineu <zhangminneu@sina.com>
2019-03-31 01:06:46 +0800

    Person

---
+++ DemoSet/Demo04LinkedHashSet.java
@@ -0,0 +1,4 @@
+package DemoSet;
+
+public class Demo04LinkedHashSet {
+}
--- DemoSet/Person.java
+++ DemoSet/Person.java
@@ -1,4 +1,59 @@
 package DemoSet;
 
+import java.util.Objects;
+
 public class Person {
+//    @Override
+//    public int hashCode() {
+//        return 1;
+//    }
+    private String name;
+    private String age;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) &&
+                Objects.equals(age, person.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
 }
