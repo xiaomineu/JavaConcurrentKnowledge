19e7754129e80854bd37e84b2ca51f4258d012c8
xiaomineu <zhangminneu@sina.com>
2019-04-05 02:30:43 +0800

    文件遍历筛选

--- File/FileDemo01/FileDemo06/DemoFile06.java
+++ File/FileDemo01/FileDemo06/DemoFile06.java
@@ -1,4 +1,32 @@
 package File.FileDemo01.FileDemo06;
 
+import java.io.File;
+
+/**
+ * 遍历文件中以.java结束的文件
+ */
 public class DemoFile06 {
+    public static void main(String[] args) {
+        File file=new File("E:\\Java_file\\offer_12\\src");
+        findAll(file);
+    }
+    private static void findAll(File file)
+    {
+        File [] strings=file.listFiles();
+        for (File f : strings) {
+            if(f.isDirectory())
+            {
+                findAll(f);
+            }
+            else
+            {
+                String string=f.getName();
+                if(string.endsWith(".java"))
+                {
+                    System.out.println(string);
+                }
+
+            }
+        }
+    }
 }
